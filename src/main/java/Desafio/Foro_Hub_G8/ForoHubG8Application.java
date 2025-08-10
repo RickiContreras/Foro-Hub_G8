95f4b597e4c02c68bb698f7901c7ceb8db599dd2
Enrique Contreras <rickiocontreras@gmail.com>
2025-08-09 22:34:30 -0300

    Primer carga del proyecto desde Foro Hub

---
+++ src/main/java/Desafio/Foro_Hub_G8/ForoHubG8Application.java
@@ -0,0 +1,13 @@
+package Desafio.Foro_Hub_G8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoHubG8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoHubG8Application.class, args);
+	}
+
+}
---
+++ src/test/java/Desafio/Foro_Hub_G8/ForoHubG8ApplicationTests.java
@@ -0,0 +1,13 @@
+package Desafio.Foro_Hub_G8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubG8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
