a033de9dffea7866f93ea17cd6be67d543a5ec7c
Enrique Contreras <rickiocontreras@gmail.com>
2025-08-13 23:33:46 -0300

    Actualiza TopicosController

--- src/main/java/Desafio/Foro_Hub_G8/controller/TopicosController.java
+++ src/main/java/Desafio/Foro_Hub_G8/controller/TopicosController.java
@@ -1,20 +1,29 @@
 package Desafio.Foro_Hub_G8.controller;
 
-import Desafio.Foro_Hub_G8.topicos.DatosCreacionTopico;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import Desafio.Foro_Hub_G8.dto.DatosCreacionTopico;
+import Desafio.Foro_Hub_G8.model.Topico;
+import Desafio.Foro_Hub_G8.service.TopicoService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/topicos")
 public class TopicosController {
 
-    @PostMapping
-    public void crearTopico (@RequestBody DatosCreacionTopico datos){
+    private final TopicoService topicoService;
 
-
-
+    // Inyección por constructor (mejor para pruebas y buenas prácticas)
+    public TopicosController(TopicoService topicoService) {
+        this.topicoService = topicoService;
     }
 
+    @PostMapping
+    public ResponseEntity<Topico> crearTopico(@RequestBody @Valid DatosCreacionTopico datos) {
+        Topico topicoCreado = topicoService.crearTopico(datos);
+        return ResponseEntity
+                .status(201) // Código HTTP 201 Created
+                .body(topicoCreado);
+    }
 }
+
