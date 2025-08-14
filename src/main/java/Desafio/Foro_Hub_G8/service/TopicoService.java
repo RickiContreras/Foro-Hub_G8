9a70caa79c7e565c2510de372404b0902549b74a
Enrique Contreras <rickiocontreras@gmail.com>
2025-08-14 15:20:33 -0300

    Actualización TopicoService

--- src/main/java/Desafio/Foro_Hub_G8/service/TopicoService.java
+++ src/main/java/Desafio/Foro_Hub_G8/service/TopicoService.java
@@ -1,9 +1,23 @@
+//package Desafio.Foro_Hub_G8.service;
+//
+//import Desafio.Foro_Hub_G8.dto.DatosCreacionTopico;
+//import Desafio.Foro_Hub_G8.model.Topico;
+//
+//public interface TopicoService {
+//    Topico crearTopico(DatosCreacionTopico datos);
+//}
+
 package Desafio.Foro_Hub_G8.service;
 
 import Desafio.Foro_Hub_G8.dto.DatosCreacionTopico;
 import Desafio.Foro_Hub_G8.model.Topico;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface TopicoService {
     Topico crearTopico(DatosCreacionTopico datos);
+    Page<Topico> obtenerTodosLosTopicos(Pageable paginacion);
+    Topico obtenerTopicoPorId(Long id);
+    Topico actualizarTopico(Long id, DatosCreacionTopico datos);
+    void eliminarTopico(Long id);
 }
-
