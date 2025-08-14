2eb1e850207fbf71dfa4f361e2bb69d93dba3542
Enrique Contreras <rickiocontreras@gmail.com>
2025-08-14 15:23:10 -0300

    Actualización TopicoService

--- src/main/java/Desafio/Foro_Hub_G8/service/TopicoService.java
+++ src/main/java/Desafio/Foro_Hub_G8/service/TopicoService.java
@@ -1,12 +1,3 @@
-//package Desafio.Foro_Hub_G8.service;
-//
-//import Desafio.Foro_Hub_G8.dto.DatosCreacionTopico;
-//import Desafio.Foro_Hub_G8.model.Topico;
-//
-//public interface TopicoService {
-//    Topico crearTopico(DatosCreacionTopico datos);
-//}
-
 package Desafio.Foro_Hub_G8.service;
 
 import Desafio.Foro_Hub_G8.dto.DatosCreacionTopico;
