627385307a12e391afe3d719eedd244e10bbf2f6
Enrique Contreras <rickiocontreras@gmail.com>
2025-08-13 23:38:15 -0300

    Actualiza TopicoRepository

---
+++ src/main/java/Desafio/Foro_Hub_G8/repository/TopicoRepository.java
@@ -0,0 +1,8 @@
+package Desafio.Foro_Hub_G8.repository;
+
+import Desafio.Foro_Hub_G8.model.Topico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long>{
+}
+
