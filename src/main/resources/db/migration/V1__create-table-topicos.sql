2558012870b831b70011d12292ea02cce3f83d4e
Enrique Contreras <rickiocontreras@gmail.com>
2025-08-14 15:19:15 -0300

    Crea V1 .sql

---
+++ src/main/resources/db/migration/V1__create-table-topicos.sql
@@ -0,0 +1,9 @@
+CREATE TABLE topicos (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    titulo VARCHAR(255) NOT NULL,
+    mensaje TEXT NOT NULL,
+    fecha_creacion DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    autor VARCHAR(255) NOT NULL,
+    curso VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
