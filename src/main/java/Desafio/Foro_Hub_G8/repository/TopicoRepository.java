3dda80e0e2b736f77a3310e2edf9c57e573e0ede
Enrique Contreras <rickiocontreras@gmail.com>
2025-08-15 09:04:15 -0300

    Actualiza TopicoRepository

--- src/main/java/Desafio/Foro_Hub_G8/repository/TopicoRepository.java
+++ src/main/java/Desafio/Foro_Hub_G8/repository/TopicoRepository.java
@@ -4,5 +4,6 @@ import Desafio.Foro_Hub_G8.model.Topico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long>{
+//     Metodo para verificar si ya existe un tópico con el mismo título y mensaje
+    boolean existsByTituloAndMensaje(String titulo, String mensaje);
 }
-
