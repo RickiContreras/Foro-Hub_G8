3a45d0bb8cba33eeefe447b190aebf7909da2e9a
Enrique Contreras <rickiocontreras@gmail.com>
2025-08-15 09:03:20 -0300

    Actualiza ForoHubG8Application

--- src/main/java/Desafio/Foro_Hub_G8/ForoHubG8Application.java
+++ src/main/java/Desafio/Foro_Hub_G8/ForoHubG8Application.java
@@ -2,12 +2,14 @@ package Desafio.Foro_Hub_G8;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@ComponentScan(basePackages = {"Desafio.Foro_Hub_G8.infra.seguridad", "Desafio.Foro_Hub_G8"})
 public class ForoHubG8Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForoHubG8Application.class, args);
 	}
-
 }
