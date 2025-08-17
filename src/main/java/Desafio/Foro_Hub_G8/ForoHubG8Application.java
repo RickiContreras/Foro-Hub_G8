74064d68a43cdd679f67df0d3aea238056ae085a
Enrique Contreras <rickiocontreras@gmail.com>
2025-08-17 10:47:43 -0300

    Actualiza ForoHubG8Application

--- src/main/java/Desafio/Foro_Hub_G8/ForoHubG8Application.java
+++ src/main/java/Desafio/Foro_Hub_G8/ForoHubG8Application.java
@@ -2,14 +2,44 @@ package Desafio.Foro_Hub_G8;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-@ComponentScan(basePackages = {"Desafio.Foro_Hub_G8.infra.seguridad", "Desafio.Foro_Hub_G8"})
+@SpringBootApplication
+@EnableWebSecurity
 public class ForoHubG8Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForoHubG8Application.class, args);
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http.csrf(csrf -> csrf.disable())
+				.sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authorizeHttpRequests(authorize -> authorize
+						.requestMatchers("/login").permitAll()
+						.anyRequest().authenticated())
+				.build();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+			throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+
+
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
