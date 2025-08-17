2735cc30d94af4bf198838ac0344e9b055f03dde
Enrique Contreras <rickiocontreras@gmail.com>
2025-08-17 11:18:58 -0300

    Actuliza LoginController

--- src/main/java/Desafio/Foro_Hub_G8/controller/LoginController.java
+++ src/main/java/Desafio/Foro_Hub_G8/controller/LoginController.java
@@ -1,6 +1,7 @@
 package Desafio.Foro_Hub_G8.controller;
 
 import Desafio.Foro_Hub_G8.dto.DatosAutenticacion;
+import Desafio.Foro_Hub_G8.infra.seguridad.TokenService;
 import Desafio.Foro_Hub_G8.model.Usuario;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -16,16 +17,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
 
     private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
 
-    public LoginController(AuthenticationManager authenticationManager) {
+    public LoginController(AuthenticationManager authenticationManager, TokenService tokenService) {
         this.authenticationManager = authenticationManager;
+        this.tokenService = tokenService;
     }
 
     @PostMapping
     public ResponseEntity autenticarUsuario(@RequestBody DatosAutenticacion datosAutenticacion) {
         Authentication authToken = new UsernamePasswordAuthenticationToken(datosAutenticacion.email(),
                 datosAutenticacion.clave());
-        authenticationManager.authenticate(authToken);
-        return ResponseEntity.ok("Usuario autenticado correctamente");
+        var usuarioAutenticado = authenticationManager.authenticate(authToken);
+        var JWTtoken = tokenService.generarToken((Usuario) usuarioAutenticado.getPrincipal());
+        return ResponseEntity.ok(JWTtoken);
     }
 }
