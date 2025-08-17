4a66eec6674f549b167221b75699d679f875ffae
Enrique Contreras <rickiocontreras@gmail.com>
2025-08-17 15:24:46 -0300

    Actuliza LoginController

--- src/main/java/Desafio/Foro_Hub_G8/controller/LoginController.java
+++ src/main/java/Desafio/Foro_Hub_G8/controller/LoginController.java
@@ -1,6 +1,43 @@
+//package Desafio.Foro_Hub_G8.controller;
+//
+//import Desafio.Foro_Hub_G8.dto.DatosAutenticacion;
+//import Desafio.Foro_Hub_G8.dto.DatosJWTToken;
+//import Desafio.Foro_Hub_G8.infra.seguridad.TokenService;
+//import Desafio.Foro_Hub_G8.model.Usuario;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/login")
+//public class LoginController {
+//
+//    private final AuthenticationManager authenticationManager;
+//    private final TokenService tokenService;
+//
+//    public LoginController(AuthenticationManager authenticationManager, TokenService tokenService) {
+//        this.authenticationManager = authenticationManager;
+//        this.tokenService = tokenService;
+//    }
+//
+//    @PostMapping
+//    public ResponseEntity autenticarUsuario(@RequestBody DatosAutenticacion datosAutenticacion) {
+//        Authentication authToken = new UsernamePasswordAuthenticationToken(datosAutenticacion.email(),
+//                datosAutenticacion.clave());
+//        var usuarioAutenticado = authenticationManager.authenticate(authToken);
+//        var JWTtoken = tokenService.generarToken((Usuario) usuarioAutenticado.getPrincipal());
+//        return ResponseEntity.ok(new DatosJWTToken(JWTtoken));
+//    }
+//}
 package Desafio.Foro_Hub_G8.controller;
 
 import Desafio.Foro_Hub_G8.dto.DatosAutenticacion;
+import Desafio.Foro_Hub_G8.dto.DatosJWTToken;
 import Desafio.Foro_Hub_G8.infra.seguridad.TokenService;
 import Desafio.Foro_Hub_G8.model.Usuario;
 import org.springframework.http.ResponseEntity;
@@ -30,6 +67,6 @@ public class LoginController {
                 datosAutenticacion.clave());
         var usuarioAutenticado = authenticationManager.authenticate(authToken);
         var JWTtoken = tokenService.generarToken((Usuario) usuarioAutenticado.getPrincipal());
-        return ResponseEntity.ok(JWTtoken);
+        return ResponseEntity.ok(new DatosJWTToken(JWTtoken, "Bearer"));
     }
 }
