3dcc629de28a10c9d1f952ba1c7a61c0f693482c
Enrique Contreras <rickiocontreras@gmail.com>
2025-08-17 10:49:17 -0300

    Crea AutenticacionService

---
+++ src/main/java/Desafio/Foro_Hub_G8/service/autenticacion/AutenticacionService.java
@@ -0,0 +1,22 @@
+package Desafio.Foro_Hub_G8.service.autenticacion;
+
+import Desafio.Foro_Hub_G8.repository.UsuarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutenticacionService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public AutenticacionService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByEmail(username);
+    }
+}
